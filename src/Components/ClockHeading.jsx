2e2b454f00d5f9f4460f8ac1f3e1b6f70274bfdf
Mahajabin_Akter_Ritu <akterritu217@gmail.com>
2025-04-21 01:08:24 +0600

    dn

---
+++ src/Components/ClockHeading.jsx
@@ -0,0 +1,6 @@
+function ClockHeading(){
+    return (
+        <h1 className="fw-bold">BD CLOCK</h1>
+    )
+}
+export default ClockHeading;
---
+++ src/Components/ClockSlogan.jsx
@@ -0,0 +1,6 @@
+function ClockSlogan(){
+    return (
+        <p className="lead">This is the clock that shows the time in Bangladesh at all times</p>
+    )
+}
+export default ClockSlogan;
